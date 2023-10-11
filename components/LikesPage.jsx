ea44b315f07210a0c7d87e6c3706464afc8f5c77
Alexis Yanguas <yanguasalexis@gmail.com>
2023-10-11 12:39:06 +0200

    🐛FIX bug LikesPage in ios

--- components/LikesPage.jsx
+++ components/LikesPage.jsx
@@ -1,50 +1,57 @@
 import { React, useState } from "react";
-import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
+import { StyleSheet, Text, View, Button, ScrollView, Dimensions } from "react-native";
 import SmallCard from "./SmallCard";
 
 export default function LikePage({users}) {
   return (
     <ScrollView style={styles.scroll_container}>
       <View style={styles.container}>
-        {users != null ? (
-          users.map((user) => (
-            <SmallCard
-              key={user.id}
-              name={user.name}
-              photo={user.photos[0]?.photo_url}
-              blurIsActive={true}
-              showName={true}
-            />
-          ))
-        ) : (
-          <View>
-            <Text
-              style={{
-                textAlign: "center",
-                fontSize: 20,
-                fontWeight: "bold",
-              }}
-            >
-              Aucun like
-            </Text>
-          </View>
-        )}
+        <View style={styles.flexContainer}>
+          {users != null ? (
+            users.map((user) => (
+              <SmallCard
+                key={user.id}
+                name={user.name}
+                photo={user.photos[0]?.photo_url}
+                blurIsActive={true}
+                showName={true}
+              />
+            ))
+          ) : (
+            <View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 20,
+                  fontWeight: "bold",
+                }}
+              >
+                Aucun like
+              </Text>
+            </View>
+          )}
+        </View>
       </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-    scroll_container: {
-        flex: 1,
-    },
-    container: {
-        flexWrap: "wrap",
-        flexDirection: "row",
-        rowGap: 20,
-        justifyContent: "space-between",
-        paddingVertical: 20,
-        paddingHorizontal: 10,
-    },
-    
+  scroll_container: {
+    flex: 1,
+  },
+  container: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  flexContainer: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    rowGap: 20,
+    justifyContent: "space-between",
+    width: 160 * 2 + 20,
+  },
 });
